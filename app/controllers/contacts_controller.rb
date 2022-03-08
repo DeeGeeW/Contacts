fad2f39b8802bcf4923b0a90465312f2ca4ebe9a
DeeGeeWest <DeeGeeWest@gmail.com>
2022-03-08 17:51:03 -0500

    bun b addition practice

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def get_one_contact
-    contact = Contact.first
+    contact = Contact.last
     # render json: {id: contact.id, title: contact.title, chef: contact.chef}
     render json: contact.as_json
   end
