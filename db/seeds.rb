9b614c048d20cf4afb9c67e1bbd63b751b8c5c91
DeeGeeWest <DeeGeeWest@gmail.com>
2022-06-27 22:16:23 -0400

    updates db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-
-
-
-Contact.create!([{first_name: "A$ap", last_name: "Rocky", email: "HarlemRepp@test.com", phone_number: "560-657-7123"},{first_name: "Mac", last_name: "Dre", email: "HyphyLyphy.com", phone_number: "319-657-7123"},{first_name: "Master", last_name: "P", email: "UhhhNANANAoNA.com", phone_number: "776-657-7123"}])
+Contact.create!([{ first_name: "A$ap", last_name: "Rocky", email: "HarlemRepp@test.com", phone_number: "560-657-7123" }, { first_name: "Mac", last_name: "Dre", email: "HyphyLyphy.com", phone_number: "319-657-7123" }, { first_name: "Master", last_name: "P", email: "UhhhNANANAoNA.com", phone_number: "776-657-7123" }, { first_name: "cheif", last_name: "Kamanch", email: "phillyphilly.com", phone_number: "s6-657-7123" }])
 # Contact.create(first_name: "A$ap", last_name: "Rocky", email: "HarlemRepp@test.com", phone_number: "560-657-7123")
-
 
 # Contact.create(first_name: "Mac", last_name: "Dre", email: "HyphyLyphy.com", phone_number: "319-657-7123")
 
-
 # Contact.create(first_name: "Master", last_name: "P", email: "UhhhNANANANA.com", phone_number: "776-657-7123")
-
-
